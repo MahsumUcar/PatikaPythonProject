8ba66221cbe1d2f3ed63a195d32b750ee0f261ff
MahsumUcar <mmahsum.ucar@gmail.com>
2022-12-08 15:50:24 +0300

    last commit

--- pythonproject.py
+++ pythonproject.py
@@ -1,3 +1,9 @@
+
+### FLATTEN
+
+x =[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+l=[]
+
 def flatten(x):
 
     for i in x:
@@ -5,14 +11,14 @@ def flatten(x):
             flatten(i)
         else:
             l.append(i)
-            
-
-x =[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
-
-l=[]
-
+        
 flatten(x)
-
-
-
 print(l)
+
+list = ["a" , "b" , "c", ["d","e"], [1, 2], [3, 4], [5, 6, 7], [8,9]]
+list.reverse()
+
+for i in range(len(list)):
+    (list[i])=(list[i])[::-1]
+
+print(list)
